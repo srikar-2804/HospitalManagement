19e18458585e7b168e2d2bb9b8561b7784eb8f39
srikar-2804 <saisrikar2804@gmail.com>
2024-12-21 18:11:13 +0530

    third commit

--- hospital/settings.py
+++ hospital/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-j8regphkwiq9a5if5a@&tva4s1#upysnds@z32yt5dq6evtl@o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://hospitalmanagement-96by.onrender.com']
 
 
 # Application definition
