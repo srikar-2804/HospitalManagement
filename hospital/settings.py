6965b443c75e1222a97b69dfe183a7b5d69a715d
srikar-2804 <saisrikar2804@gmail.com>
2024-12-21 18:13:29 +0530

    fourth commit

--- hospital/settings.py
+++ hospital/settings.py
@@ -85,7 +85,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitnoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     
     
 
